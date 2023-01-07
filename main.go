90238d0d4bc186a11b00ff7376c07492d46a9a36
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-01-07 12:37:51 +0530

    register handler func for About page

--- main.go
+++ main.go
@@ -9,9 +9,14 @@ func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "This is the Go Home Page")
 }
 
+func About(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "This is the Go About Page")
+}
+
 func main() {
 	// listens for a request sent by a web browser
 	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
 
 	// start server to listen for request
 	_ = http.ListenAndServe(":8080", nil)
