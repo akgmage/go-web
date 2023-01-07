ea4bad7b35e24b41790f40323404e415c8e89463
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-01-07 12:24:13 +0530

    initial commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	// listens for a request sent by a web browser
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		n, err := fmt.Fprintf(w, "Hello World!")
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Printf(fmt.Sprintf("Number of bytes written: %d", n))
+	})
+	
+	// start server
+	_ = http.ListenAndServe(":8080", nil)
+}
