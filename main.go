f82529ef3616b1e3390165570b683eaf4babe91d
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-01-08 20:28:08 +0530

    add renderTemplate func which takes a responsewriter and name of template, parse it and write it to browser window

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"text/template"
 )
 
 const portNumber = ":8080"
@@ -20,6 +21,16 @@ func About(w http.ResponseWriter, r *http.Request) {
 
 }
 
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("Error parsing template:", err)
+		return
+	}
+
+}
 
 
 
