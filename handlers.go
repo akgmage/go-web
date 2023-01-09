e2f16fcd12170acb738b7520b8d1151b24dd2917
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-01-09 19:12:20 +0530

    refactor: move renderTemplate into separate file

--- handlers.go
+++ handlers.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	"text/template"
 )
 
 // Home is the home page handler
@@ -16,15 +14,4 @@ func About(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "about.page.tmpl")
 }
 
-// renderTemplate takes a response writer , name of template, parse it and
-// write it to browser window
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	// load file from root of application
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("Error parsing template:", err)
-		return
-	}
 
-}
---
+++ render.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+// renderTemplate takes a response writer , name of template, parse it and
+// write it to browser window
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	// load file from root of application
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("Error parsing template:", err)
+		return
+	}
+
+}
