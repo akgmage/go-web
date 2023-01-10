0ee4d5a7870eb5462abb636a83b8ceac6a76518d
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-01-10 19:38:10 +0530

    add RenderTempalte2 func which will read from cache is template exists

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -18,3 +18,19 @@ func RenderTemplate(w http.ResponseWriter, tmpl string) {
 	}
 
 }
+
+tc := make(map[string]template.Template)
+
+func RenderTempalte2(w http.ResponseWriter, t string) {
+	var tmpl *template.Template
+	var err error
+
+	_, inMap := tc[t]
+	if !inMap {
+		// create the template
+	} else {
+		log.Println("Using cached template")
+	}
+	tmpl = tc[t]
+	err = tmpl.Execute(w, nil)
+}
