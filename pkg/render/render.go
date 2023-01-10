66bd9b0694e8179a168840ab074ed663cf210d6f
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-01-10 19:42:57 +0530

    add createTemplateCache func

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -19,7 +19,7 @@ func RenderTemplate(w http.ResponseWriter, tmpl string) {
 
 }
 
-tc := make(map[string]template.Template)
+var tc = make(map[string]*template.Template)
 
 func RenderTempalte2(w http.ResponseWriter, t string) {
 	var tmpl *template.Template
@@ -33,4 +33,20 @@ func RenderTempalte2(w http.ResponseWriter, t string) {
 	}
 	tmpl = tc[t]
 	err = tmpl.Execute(w, nil)
+}
+
+func createTemplateCache(t string) error {
+	templates := []string{
+		fmt.Sprintf("./templates/%s", t),
+		"./templates/base.layout.tmpl",
+	}
+
+	// parse the template
+	tmpl, err := template.ParseFiles(templates...)
+	if err != nil {
+		return err
+	}
+	// add template to cache
+	tc[t] = tmpl
+	return nil
 }
