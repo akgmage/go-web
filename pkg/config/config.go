4263deeb54c6477a07ee7a299068628e1e923465
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-01-11 23:01:30 +0530

    add comment

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -3,6 +3,7 @@ package config
 import "html/template"
 
 // AppConfig holds the application config
+// immediately available to every other part of the application
 type AppConfig struct {
 	UseCache bool
 	TemplateCache map[string]*template.Template
