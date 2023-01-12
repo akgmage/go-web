d280ca68910e682bc22e3cbd288740110f520df9
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-01-12 23:00:40 +0530

    register a pattern with a handler for About page

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -12,5 +12,6 @@ func routes(app *config.AppConfig) http.Handler {
 	mux := pat.New()
 
 	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
 	return mux
 }
