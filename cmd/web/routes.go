1d39b2783ef6eac1726b8e4895b3de4f87697429
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-01-12 23:00:08 +0530

    register a pattern with a handler for Home

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -4,11 +4,13 @@ import (
 	"net/http"
 
 	"github.com/akgmage/go-web/pkg/config"
+	"github.com/akgmage/go-web/pkg/handlers"
 	"github.com/bmizerany/pat"
 )
 
 func routes(app *config.AppConfig) http.Handler {
 	mux := pat.New()
 
+	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
 	return mux
 }
