b18871254732f3b9f2352ac52443202991bc98ce
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-01-13 22:36:32 +0530

    add NoSurf func to create NoSurf token

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -3,12 +3,25 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/justinas/nosurf"
 )
 
-// WriteToConsole writes to console and moves to next middleware 
+// WriteToConsole writes to console and moves to next middleware
 func WriteToConsole(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("hit the page")
 		next.ServeHTTP(w, r)	
+	})
+}
+
+func NoSurf(next http.Handler) http.Handler {
+	csrfhandler := nosurf.New(next)
+
+	csrfhandler.SetBaseCookie(http.Cookie{
+		HttpOnly: true,
+		Path: "/",
+		Secure: false,
+		SameSite: http.SameSiteLaxMode,
 	})
 }
