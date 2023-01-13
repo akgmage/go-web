3a24cf1451891505aff4eab6849fea42321d4c55
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-01-13 22:22:49 +0530

    add a middleware that prints to console

---
+++ cmd/web/middleware.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func WriteToConsole(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("hit the page")
+		next.ServeHTTP(w, r)	
+	})
+}
