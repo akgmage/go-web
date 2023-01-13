7c61bb794f5698662c74960b7847a06d3554ae91
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-01-13 22:13:45 +0530

    register a pattern with a handler for Home and About page

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -5,14 +5,18 @@ import (
 
 	"github.com/akgmage/go-web/pkg/config"
 	"github.com/akgmage/go-web/pkg/handlers"
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	// mux := pat.New()
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
 	
+
+	mux := chi.NewRouter()
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 	return mux
 }
