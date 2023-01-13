5668bd0d9a31586c8d49d1d514959fde91fdc22d
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-01-13 22:14:08 +0530

    clean up code

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -9,12 +9,6 @@ import (
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	// mux := pat.New()
-
-	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
-	
-
 	mux := chi.NewRouter()
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
