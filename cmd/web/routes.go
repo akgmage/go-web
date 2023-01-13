dca0050b243c8f00c9969e8d0d256081c1d45085
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-01-13 22:16:37 +0530

    add a recoverer  middleware that recovers from panics

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -6,10 +6,14 @@ import (
 	"github.com/akgmage/go-web/pkg/config"
 	"github.com/akgmage/go-web/pkg/handlers"
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 func routes(app *config.AppConfig) http.Handler {
 	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 	return mux
