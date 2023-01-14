a69c0f8d6e07acfb8e9ba391ae6134dee84fa123
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-01-14 20:07:04 +0530

    if somebody hits the home page, for that user session store remote IP

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -30,6 +30,10 @@ func Newhandlers(r *Repository) {
 // Home is the home page handler
 // (m *Repository) with this receiver we have access to everything inside Repository i.e application config
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
 	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
 }
 
