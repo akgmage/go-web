32c946b4f2edc0d70eeba1bd2ea299ecf939771d
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-01-14 20:15:18 +0530

    add comment

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -42,10 +42,12 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	// perform some logic 
 	stringMap := make(map[string]string)
 	stringMap["test"] = "Hello, again."
-	// send data to the template
+	
+	// pull ip address
 	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
 	stringMap["remote_ip"] = remoteIP
 
+	// send data to the template
 	render.RenderTemplate(w, "about.page.tmpl",  &models.TemplateData{
 		StringMap: stringMap,
 	})
