55d67a70b49dc7255306013471b86f7dfc4b8ca0
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-01-14 19:41:06 +0530

    add InProduction anf InfoLog in AppConfig

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,10 +1,15 @@
 package config
 
-import "html/template"
+import (
+	"html/template"
+	"log"
+)
 
 // AppConfig holds the application config
 // immediately available to every other part of the application
 type AppConfig struct {
 	UseCache bool
 	TemplateCache map[string]*template.Template
+	InfoLog *log.Logger
+	InProduction bool
 }
